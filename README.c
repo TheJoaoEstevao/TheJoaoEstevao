9be3937cc21ffd600de98c821152c828809ff8e0
João Estêvão <joao8estevao@gmail.com>
2024-09-06 10:35:39 +0100

    Update README.c

--- README.c
+++ README.c
@@ -1,17 +1,1 @@
-#include <unistd.h>
 
-int main(void)
-
-{
-
-     write(1, "👋 Hi, I’m @TheJoaoEstevao", 23); 
-  
-     write(1, "👀 I’m interested in creating and learning code!", 40);
-  
-     write(1, "🌱 I’m currently learning C!", 29);
-  
-     write(1, "💞️ I’m looking to collaborate on anything and everything!", 50);
-  
-     write(1, "📫 How to reach me : joao8estevao@gmail.com", 35);
-  
-}
